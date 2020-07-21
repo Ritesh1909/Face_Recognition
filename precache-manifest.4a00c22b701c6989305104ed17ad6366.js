09549511a6c33bc6f7f187a438ebc30131e5337e
Ritesh1909 <30571983+Ritesh1909@users.noreply.github.com>
2020-07-22 01:52:38 +0530

    Updates

---
+++ precache-manifest.4a00c22b701c6989305104ed17ad6366.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a3a0883a31e6fcba06fdbe60205593",
+    "url": "/Face_Recognition/index.html"
+  },
+  {
+    "revision": "7b1a3a690eda566b25d1",
+    "url": "/Face_Recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bc7b60137c6f1cab3dc1",
+    "url": "/Face_Recognition/static/css/main.5fd4e26f.chunk.css"
+  },
+  {
+    "revision": "7b1a3a690eda566b25d1",
+    "url": "/Face_Recognition/static/js/2.8c63625d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face_Recognition/static/js/2.8c63625d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7b60137c6f1cab3dc1",
+    "url": "/Face_Recognition/static/js/main.479160e6.chunk.js"
+  },
+  {
+    "revision": "53bfa22a080015839fc1",
+    "url": "/Face_Recognition/static/js/runtime-main.ef2df322.js"
+  },
+  {
+    "revision": "09d5db499330f17007259562e9c963d5",
+    "url": "/Face_Recognition/static/media/background1.09d5db49.jpg"
+  }
+]);
