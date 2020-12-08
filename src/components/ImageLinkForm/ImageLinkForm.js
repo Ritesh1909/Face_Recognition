4b4ba2fdbce51eb6e18443f3a56c2647cd755ba9
Ritesh Sharma <riteshsharma1909@gmail.com>
2020-12-08 22:07:24 +0530

    Update ImageLinkForm.js
    putting placeholder

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -13,7 +13,7 @@ const ImageLinkForm =({ onInputChange , onButtonSubmit })=>{
 	
 	<div className='center'>
 		<div className='form center pa4 br3 shadow-5'>
-			<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+			<input className='f4 pa2 w-70 center' type='text' placeholder='Enter Image URL' onChange={onInputChange}/>
 			<button 
 			className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 			onClick={onButtonSubmit}
